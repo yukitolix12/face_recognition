b3fd519fca95835f568ed7117a9f7257c05e402a
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-12-08 23:45:27 +0900

    optionsの変数を用意する

--- face-hand-tracking.js
+++ face-hand-tracking.js
@@ -5,6 +5,10 @@ const canvas = document.getElementById("mycanvas");
 
 let model;
 
+const options = {
+    
+}
+
 handTrack.load(options).then(function(modelData){
     model = modelData;
     console.log(model);
